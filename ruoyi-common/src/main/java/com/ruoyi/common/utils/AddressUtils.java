b50e2722d1c0052895f6956126c366e542d147e8
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-01-21 15:57:11 +0800

    //取消获取ip地址 占用io过多

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/AddressUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/AddressUtils.java
@@ -24,7 +24,7 @@ public class AddressUtils {
         if (IpUtils.internalIp(ip)) {
             return "内网IP";
         }
-        if (Global.isAddressEnabled()) {
+        /*if (Global.isAddressEnabled()) {
             String rspStr = HttpUtils.sendPost(IP_URL, "ip=" + ip);
             if (StringUtils.isEmpty(rspStr)) {
                 log.error("获取地理位置异常 {}", ip);
@@ -40,7 +40,7 @@ public class AddressUtils {
             } catch (Exception e) {
                 log.error("获取地理位置异常 {}", ip);
             }
-        }
+        }*/
         return address;
     }
 }
