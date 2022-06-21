7019f9c09694757ed59a8177ac13564d93cfc8d9
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-06-21 21:32:41 +0800

    //将历史数据分别加到 商户/卡商 下面

---
+++ sql/new.sql
@@ -0,0 +1,6 @@
+INSERT INTO sys_menu
+(menu_id, menu_name, parent_id, order_num, url, target, menu_type, visible, perms, icon, create_by, create_time, update_by, update_time, remark)
+VALUES(2302, '卡商历史数据', 2007, 99, '/alipay/fund/bak?userType=2', 'menuItem', 'C', '0', NULL, 'fa fa-bar-chart', 'admin', '2022-06-21 21:23:05', '', NULL, '');
+INSERT INTO sys_menu
+(menu_id, menu_name, parent_id, order_num, url, target, menu_type, visible, perms, icon, create_by, create_time, update_by, update_time, remark)
+VALUES(2303, '商户历史数据', 2006, 99, '/alipay/fund/bak?userType=1', 'menuItem', 'C', '0', NULL, '#', 'admin', '2022-06-21 21:23:33', '', NULL, '');
