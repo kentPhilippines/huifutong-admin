413692dc4b5a7cd97e6b8aab613f32c0f87bf02e
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-07-07 15:23:28 +0800

    //bankconfig 银行编码配置

---
+++ ruoyi-system/src/main/java/com/ruoyi/alipay/service/impl/AlipayBankConfigServiceImpl.java
@@ -0,0 +1,97 @@
+package com.ruoyi.alipay.service.impl;
+
+import com.ruoyi.alipay.domain.AlipayBankConfig;
+import com.ruoyi.alipay.mapper.AlipayBankConfigMapper;
+import com.ruoyi.alipay.service.IAlipayBankConfigService;
+import com.ruoyi.common.core.text.Convert;
+import com.ruoyi.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * bankconfigService业务层处理
+ * 
+ * @author ruoyi
+ * @date 2022-07-07
+ */
+@Service
+public class AlipayBankConfigServiceImpl implements IAlipayBankConfigService
+{
+    @Autowired
+    private AlipayBankConfigMapper alipayBankConfigMapper;
+
+    /**
+     * 查询bankconfig
+     * 
+     * @param id bankconfigID
+     * @return bankconfig
+     */
+    @Override
+    public AlipayBankConfig selectAlipayBankConfigById(Integer id)
+    {
+        return alipayBankConfigMapper.selectAlipayBankConfigById(id);
+    }
+
+    /**
+     * 查询bankconfig列表
+     * 
+     * @param alipayBankConfig bankconfig
+     * @return bankconfig
+     */
+    @Override
+    public List<AlipayBankConfig> selectAlipayBankConfigList(AlipayBankConfig alipayBankConfig)
+    {
+        return alipayBankConfigMapper.selectAlipayBankConfigList(alipayBankConfig);
+    }
+
+    /**
+     * 新增bankconfig
+     * 
+     * @param alipayBankConfig bankconfig
+     * @return 结果
+     */
+    @Override
+    public int insertAlipayBankConfig(AlipayBankConfig alipayBankConfig)
+    {
+        alipayBankConfig.setCreateTime(DateUtils.getNowDate());
+        return alipayBankConfigMapper.insertAlipayBankConfig(alipayBankConfig);
+    }
+
+    /**
+     * 修改bankconfig
+     * 
+     * @param alipayBankConfig bankconfig
+     * @return 结果
+     */
+    @Override
+    public int updateAlipayBankConfig(AlipayBankConfig alipayBankConfig)
+    {
+        return alipayBankConfigMapper.updateAlipayBankConfig(alipayBankConfig);
+    }
+
+    /**
+     * 删除bankconfig对象
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    @Override
+    public int deleteAlipayBankConfigByIds(String ids)
+    {
+        return alipayBankConfigMapper.deleteAlipayBankConfigByIds(Convert.toStrArray(ids));
+    }
+
+    /**
+     * 删除bankconfig信息
+     * 
+     * @param id bankconfigID
+     * @return 结果
+     */
+    @Override
+    public int deleteAlipayBankConfigById(Integer id)
+    {
+        return alipayBankConfigMapper.deleteAlipayBankConfigById(id);
+    }
+}
