2e4ea0619b0535f63aaa5c6229ce5f35d47047ae
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-09-13 10:33:54 +0800

    //不知道为什么sql后面多个单引号

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/alipay/MerchantInfoEntityController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/alipay/MerchantInfoEntityController.java
@@ -198,17 +198,14 @@ public class MerchantInfoEntityController extends BaseController {
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable("id") Long id, ModelMap mmap) {
         AlipayUserInfo userInfo = merchantInfoEntityService.selectMerchantInfoEntityById(id);
-      // List<String> str = new ArrayList();
-      //  str.add(userInfo.getUserId());
-      //  List<SysUser> sysUsers = userService.selectUserByLoginNames(str);
-        SysUser u =  userService.findMerchant(userInfo.getUserId(),0);
-        userInfo.setRemark(u.getRemark());
-/*
+        List<String> str = new ArrayList();
+        str.add(userInfo.getUserId());
+        List<SysUser> sysUsers = userService.selectUserByLoginNames(str);
         //过滤accounttype=0
         sysUsers = sysUsers.stream().filter(u->u.getAccountType()!=null &&u.getAccountType()==0).collect(Collectors.toList());
         for (SysUser sys : sysUsers) {
             userInfo.setRemark(sys.getRemark());
-        }*/
+        }
         mmap.put("alipayUserInfo", userInfo);
         return prefix + "/edit";
     }
