3ea711bbf515850467df9526dad6d5495f57cf3c
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-09-22 12:08:30 +0800

    //新增配置中心

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,9 +1,9 @@
 package com.ruoyi;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -12,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableScheduling //轻量级定时任务
+@NacosPropertySource(dataId = "${nacos.config.data-id}",autoRefreshed = true)
 public class RuoYiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RuoYiApplication.class, args);
