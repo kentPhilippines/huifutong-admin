64b46affeb3eec8f3f4c7ad367468784d7731489
jacksmithphi@gmail.com <jacksmithphi@gmail.com>
2022-09-30 23:39:56 +0800

    //催单 和 单卡接单冷却时间sql

--- sql/new.sql
+++ sql/new.sql
@@ -10,3 +10,9 @@ VALUES(2303, '商户历史数据', 2006, 99, '/alipay/fund/bak?userType=1', 'men
 
 ALTER TABLE alipay_medium ADD maxAmount decimal(19,6) NULL;
 ALTER TABLE alipay_medium MODIFY COLUMN maxAmount decimal(19,6) DEFAULT 0 NULL;
+
+--催单
+ALTER TABLE alipay_deal_order ADD urge INT UNSIGNED DEFAULT 0 NULL COMMENT '催单，1是催单，0是正常';
+--单卡冷却时间
+ALTER TABLE alipay_medium ADD coolDownTime INT UNSIGNED DEFAULT 30 NOT NULL COMMENT '冷却时间30秒内只可接一单';
+
